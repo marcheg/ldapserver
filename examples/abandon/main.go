f7462844ec26b9df77c671f7282350e67c34a6f8
Marek <marek.kocan@silverengine.de>
2018-04-20 19:02:26 +0200

    adapting examples to correct references

--- examples/abandon/main.go
+++ examples/abandon/main.go
@@ -7,7 +7,7 @@ import (
 	"syscall"
 	"time"
 
-	ldap "github.com/vjeantet/ldapserver"
+	ldap "github.com/marcheg/ldapserver"
 )
 
 func main() {
--- examples/complex/main.go
+++ examples/complex/main.go
@@ -8,7 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	ldap "github.com/vjeantet/ldapserver"
+	ldap "github.com/marcheg/ldapserver"
 )
 
 func main() {
